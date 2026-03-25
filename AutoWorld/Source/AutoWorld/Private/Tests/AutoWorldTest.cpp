798ae400e8fb8ef92f986e67749800cce667a541
이정훈 <eu4ng97@gmail.com>
2026-03-25 23:17:10 +0900

    Merge pull request #1 from E4-Unreal/feature/automation-test
    feat: 언리얼 엔진 자동 테스트 시스템 구축

---
+++ AutoWorld/Source/AutoWorld/Private/Tests/AutoWorldTest.cpp
@@ -0,0 +1,10 @@
+﻿#include "Misc/AutomationTest.h"
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(AutoWorldTest, "AutoWorld.AutoWorld.AutoWorldTest",
+                                 EAutomationTestFlags::EditorContext | EAutomationTestFlags::EngineFilter)
+
+bool AutoWorldTest::RunTest(const FString& Parameters)
+{
+    // Make the test pass by returning true, or fail by returning false.
+    return false;
+}
